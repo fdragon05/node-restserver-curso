c2e46ea04da6575947ac8855416fa5d91c01de79
sergio <smondragon4321@gmail.com>
2021-01-14 11:12:43 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,23 @@
 //=======================
 // Puerto
 //=======================
-
-
 process.env.PORT = process.env.PORT || 3000;
+
+//=======================
+// Entorno
+//=======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=======================
+// Base de Datos
+//=======================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://strider:AV1nVno9HN8UwKUH@cluster0.nja8v.mongodb.net/cafe';
+}
+process.env.urlDB = urlDB;
+
+
--- server/server.js
+++ server/server.js
@@ -1,51 +1,28 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-})
-
-app.post('/usuario', function (req, res) {
-
-  let body = req.body;
-
-  if( body.nombre === undefined){
-
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-
-  }else{
-
-    res.json({
-      persona: body
-    });
-  }
+app.use( require('./routes/usuario'));
  
-})
+mongoose.connect(process.env.urlDB,
+                { useNewUrlParser: true, useCreateIndex: true},
+                ( err, res ) =>{
+    
+  if(err) throw new err;
 
-app.put('/usuario/:id', function (req, res) {
+  console.log('Base de datos ONLINE');
+});
 
-  let id = req.params.id;
-  res.json({
-    id
-  })
-})
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario')
-})
- 
 app.listen(process.env.PORT, () =>{
     console.log('Escuchado en el puerto', 3000)
 })
